c917fceadf6f3de8926d2b0f33d97eed0a3cdd85
matt burnett <mawburn@users.noreply.github.com>
2021-12-19 21:47:40 -0500

    Update index.tsx

--- packages/coresite/pages/index.tsx
+++ packages/coresite/pages/index.tsx
@@ -12,6 +12,14 @@ const Home: NextPage = () => (
     </Head>
     <Header />
     <main>
+      <section>
+        <h2 className="my-2 p-0 mx-0 font-tgt text-center text-xl">The Dream</h2>
+        <p>The idea behind this project, will be to build out simple or complex tools that help with various games. It is meant to be an open source community effort, so feel free to contribute or add to the list.</p>
+        <p>This repo used NextJS, Typescript, &amp; Tailwind.css. It is in it's early stages, but we will build up a repo of helpful tools &amp; components to speed up the process of making useful tools. As well as provide a simple yet opinionated approach.</p>
+        <p>But the tools don't need to be created as part of this repo or even use the same libraries or languages. Each toolset will be given its own subdomain, hence the name of the site.</p>
+        <p className="text-center">ie: <strong>https://[gamename].the game.tools</strong></p>
+      </section>
+      <section>
       <h2 className="my-2 p-0 mx-0 font-tgt text-center text-xl">List of Tools</h2>
       <div className="flex justify-center items-center">
         <GameLink
@@ -21,6 +29,7 @@ const Home: NextPage = () => (
           descr="A random world generator for the game PbtA game Fluxfall Horizon"
         />
       </div>
+      </section>
     </main>
   </main>
 )
